0d399854d6a9fd64249053a94432257624f1d32e
alvaro220592 <alvaro220592@gmail.com>
2023-03-21 21:21:08 -0300

    criação de tabelas de endereço

---
+++ database/migrations/2023_03_22_001040_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\City;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('logradouro');
+            $table->foreignIdFor(City::class)->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
