457b67649a93703a46e97e1dad6ff9977de3a061
alvaro220592 <alvaro220592@gmail.com>
2023-04-04 22:52:05 -0300

    Melhoria rascunho dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,49 +16,9 @@
         <!-- <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
         </template> -->
-
-                    
-                    <!-- <div class="flex flex-col p-2">
-                        <div class="overflow-x-auto bg-white">
-                            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                                <div class="overflow-hidden">
-                                    <table class="min-w-full"> -->
-                    
-                        <div class="overflow-x-auto bg-white scroll_div">
-                            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                                <div class="overflow-x-auto">
-                                    <table class="min-w-full">
-                                        <thead class="border-b">
-                                            <tr>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">aaaaaaaa</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr class="border-b">
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">bbbbbbbbb</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
+        <h1 class="text-2xl mb-4">Dashboard</h1>
+        <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quidem doloribus deserunt nulla veritatis nostrum et animi nam velit? Minima reiciendis facilis necessitatibus ut natus amet, dolorem repellat consequatur tempore.</p>
+        <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quidem doloribus deserunt nulla veritatis nostrum et animi nam velit? Minima reiciendis facilis necessitatibus ut natus amet, dolorem repellat consequatur tempore.</p>
+        <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quidem doloribus deserunt nulla veritatis nostrum et animi nam velit? Minima reiciendis facilis necessitatibus ut natus amet, dolorem repellat consequatur tempore.</p>
     <!-- </AuthenticatedLayout> -->
 </template>
